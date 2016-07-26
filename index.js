7215b55e765d398921b499b674aa700647cb4d7c
Marlin Gingerich <marlin.gingerich@gmail.com>
2016-07-26 00:35:00 -0400

    Adding support for array of conditions

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 'use strict';
 var url = require('url');
 
-module.exports = function(options) {
+function unless(options) {
   var parent = this;
 
-  var opts = typeof options === 'function' ? {custom: options} : options;
-  opts.useOriginalUrl = (typeof opts.useOriginalUrl === 'undefined') ? true : opts.useOriginalUrl;
+  var opts = typeof options === 'function' ? [{custom: options}] : coerceToArray(options);
 
-  return function(ctx, next) {
-    var requestedUrl = url.parse((opts.useOriginalUrl ? ctx.originalUrl : ctx.url) || '', true);
+  function middleware(ctx, next) {
+    var skip = opts.reduce(function(skip, opts) {
+      opts.useOriginalUrl = (typeof opts.useOriginalUrl === 'undefined') ? true : opts.useOriginalUrl;
+      var requestedUrl = url.parse((opts.useOriginalUrl ? ctx.originalUrl : ctx.url) || '', true);
 
-    var skip = false;
+      if (opts.custom) {
+        skip = skip || opts.custom(ctx);
+      }
 
-    if (opts.custom) {
-      skip = skip || opts.custom(ctx);
-    }
+      var paths = !opts.path || Array.isArray(opts.path) ?
+                  opts.path : [opts.path];
 
-    var paths = !opts.path || Array.isArray(opts.path) ?
-                opts.path : [opts.path];
+      if (paths) {
+        skip = skip || paths.some(function(p) {
+          return (typeof p === 'string' && p === requestedUrl.pathname) ||
+            (p instanceof RegExp && !! p.exec(requestedUrl.pathname));
+        });
+      }
 
-    if (paths) {
-      skip = skip || paths.some(function(p) {
-        return (typeof p === 'string' && p === requestedUrl.pathname) ||
-          (p instanceof RegExp && !! p.exec(requestedUrl.pathname));
-      });
-    }
+      var exts = !opts.ext || Array.isArray(opts.ext) ?
+                 opts.ext : [opts.ext];
 
-    var exts = !opts.ext || Array.isArray(opts.ext) ?
-               opts.ext : [opts.ext];
+      if (exts) {
+        skip = skip || exts.some(function(ext) {
+           return requestedUrl.pathname.substr(ext.length * -1) === ext;
+        });
+      }
 
-    if (exts) {
-      skip = skip || exts.some(function(ext) {
-         return requestedUrl.pathname.substr(ext.length * -1) === ext;
-      });
-    }
+      var methods = !opts.method || Array.isArray(opts.method) ?
+                    opts.method : [opts.method];
 
-    var methods = !opts.method || Array.isArray(opts.method) ?
-                  opts.method : [opts.method];
+      if (methods) {
+        skip = skip || !!~methods.indexOf(ctx.method);
+      }
 
-    if (methods) {
-      skip = skip || !!~methods.indexOf(ctx.method);
-    }
+      return skip
+    }, false);
 
     if (skip) {
       return next();
@@ -48,4 +50,13 @@ module.exports = function(options) {
 
     return parent(ctx, next);
   };
+
+  middleware.or = unless;
+  return middleware;
 };
+
+function coerceToArray(val) {
+  return Array.isArray(val) ? val : [val];
+}
+
+module.exports = unless;
